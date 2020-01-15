279acda9e5d121dabde229e59604989306b1d70f
Thanga Ganapathy <ganapathy888@gmail.com>
2020-01-15 23:22:00 +0530

    Init app with React, Babel and Webpack

---
+++ webpack.common.js
@@ -0,0 +1,23 @@
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: {
+    app: "./src/index.js"
+  },
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: "Tharavu",
+      template: "src/index.html"
+    })
+  ],
+  module: {
+    rules: [{ test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }]
+  }
+};
