cb74140cd0df81916806b0db117cba0443ad14d5
Thanga Ganapathy <ganapathy888@gmail.com>
2020-02-06 17:08:13 +0530

    Remove MUI theme provider

--- src/App.js
+++ src/App.js
@@ -1,14 +1,7 @@
 import React from 'react';
-import { ThemeProvider } from '@material-ui/core/styles';
 
 import Routes from './routes';
 
-const themeInstance = {};
-
 export default function App() {
-  return (
-    <ThemeProvider theme={themeInstance}>
-      <Routes />
-    </ThemeProvider>
-  );
+  return <Routes />;
 }
