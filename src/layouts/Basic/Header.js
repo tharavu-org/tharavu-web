1876aacc2846b8898b281b031eacde3a64197be2
Thanga Ganapathy <ganapathy888@gmail.com>
2020-02-07 11:09:39 +0530

    Add sign in button to header

--- src/layouts/Basic/Header.js
+++ src/layouts/Basic/Header.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { AppBar, Toolbar, Typography } from '@material-ui/core';
+import { AppBar, Toolbar, Typography, Button } from '@material-ui/core';
 
 const useStyles = makeStyles({
   appBar: {
     height: '50px',
+  },
+  title: {
+    flexGrow: 1,
   },
 });
 
@@ -13,7 +16,12 @@ export default function Header() {
   return (
     <AppBar position="fixed" color="default" className={classes.appBar}>
       <Toolbar variant="dense">
-        <Typography variant="h6">தரவு</Typography>
+        <Typography variant="h6" className={classes.title}>
+          தரவு
+        </Typography>
+        <Button variant="contained" size="small" color="primary">
+          Sign in
+        </Button>
       </Toolbar>
     </AppBar>
   );
