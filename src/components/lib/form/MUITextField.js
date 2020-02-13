34e8a3a61e010567855b3db12217a2d46011c50a
Thanga Ganapathy <ganapathy888@gmail.com>
2020-02-13 20:06:21 +0530

    Add req prop to text field

--- src/components/lib/form/MUITextField.js
+++ src/components/lib/form/MUITextField.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import TextField from '@material-ui/core/TextField';
 
-export default function MUITextField({ type, label, onChange, value }) {
+export default function MUITextField(props) {
+  const { type, label, onChange, value, required } = props;
   return (
     <TextField
+      fullWidth
       label={label}
       variant="outlined"
       type={type}
       onChange={onChange}
       value={value}
+      required={required}
     />
   );
 }
