ff7fe290f15625fd2fea95c4514817602baee21c
Thanga Ganapathy <ganapathy888@gmail.com>
2020-02-13 20:11:20 +0530

    Add sign in form

---
+++ src/components/app/SignIn/Form.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Form as ReactForm, Field } from '@open-tech-world/react-form';
+import { Button, Box } from '@material-ui/core';
+
+import MUITextField from '../../lib/form/MUITextField';
+
+export default function Form() {
+  return (
+    <ReactForm>
+      <Box display="flex" flexDirection="column">
+        <Box display="flex" m={2}>
+          <Field
+            component={MUITextField}
+            name="email"
+            label="Email Address"
+            required
+          />
+        </Box>
+        <Box display="flex" m={2}>
+          <Field
+            component={MUITextField}
+            name="password"
+            label="Password"
+            type="password"
+            required
+          />
+        </Box>
+        <Box display="flex" flexDirection="row-reverse" m={2}>
+          <Button type="submit" variant="contained" color="primary">
+            Sign In
+          </Button>
+        </Box>
+      </Box>
+    </ReactForm>
+  );
+}
---
+++ src/components/app/SignIn/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from '@material-ui/core';
+
+import Form from './Form';
+
+export default function SignIn() {
+  return (
+    <Container maxWidth="xs">
+      <Form />
+    </Container>
+  );
+}
