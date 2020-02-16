6c1db14d98b40bec87105465a2f5cd1acfd7dcde
Thanga Ganapathy <ganapathy888@gmail.com>
2020-02-16 18:24:40 +0530

    Add redux logger middleware

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -3,6 +3,14 @@ import rootReducer from './reducers';
 
 export default function configureStore(preloadedState) {
   const middlewares = [];
+  if (process.env.NODE_ENV === `development`) {
+    // eslint-disable-next-line global-require
+    const { createLogger } = require(`redux-logger`);
+    const loggerMiddleware = createLogger({
+      collapsed: true,
+    });
+    middlewares.push(loggerMiddleware);
+  }
   const middlewareEnhancer = applyMiddleware(...middlewares);
   const composedEnhancers = compose(middlewareEnhancer);
   const store = createStore(rootReducer, preloadedState, composedEnhancers);
