1ce6ed748c095076292b8581cc111f6fb0cf9d5f
Thanga Ganapathy <ganapathy888@gmail.com>
2020-02-17 15:47:14 +0530

    Hide signin dialog & btn after sign in success

--- src/layouts/Basic/Header.js
+++ src/layouts/Basic/Header.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
 import { makeStyles } from '@material-ui/core/styles';
 import { AppBar, Toolbar, Typography, Button } from '@material-ui/core';
 
@@ -16,29 +17,46 @@ const useStyles = makeStyles({
 
 export default function Header() {
   const [dialogOpen, setDialogOpen] = useState(false);
-
+  const currentUser = useSelector(state => state.session.currentUser);
   const classes = useStyles();
+
+  const renderSigninBtn = () => {
+    if (currentUser) {
+      return (
+        <Button variant="text" size="small" color="secondary">
+          Sign out
+        </Button>
+      );
+    }
+
+    return (
+      <Button
+        variant="outlined"
+        size="small"
+        color="primary"
+        onClick={() => setDialogOpen(true)}
+      >
+        Sign in
+      </Button>
+    );
+  };
+
   return (
     <AppBar position="fixed" color="default" className={classes.appBar}>
       <Toolbar variant="dense">
         <Typography variant="h6" className={classes.title}>
           தரவு
         </Typography>
-        <Button
-          variant="outlined"
-          size="small"
-          color="primary"
-          onClick={() => setDialogOpen(true)}
-        >
-          Sign in
-        </Button>
-        <AppDialog
-          open={dialogOpen}
-          title="Sign in"
-          onClose={() => setDialogOpen(false)}
-        >
-          <SignIn />
-        </AppDialog>
+        {renderSigninBtn()}
+        {!currentUser && (
+          <AppDialog
+            open={dialogOpen}
+            title="Sign in"
+            onClose={() => setDialogOpen(false)}
+          >
+            <SignIn />
+          </AppDialog>
+        )}
       </Toolbar>
     </AppBar>
   );
