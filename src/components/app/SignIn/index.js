7a944567f2d9e539fdbaab97b5d828a923ce0760
Thanga Ganapathy <ganapathy888@gmail.com>
2020-02-17 13:18:28 +0530

    Dispatch signin form values

--- src/components/app/SignIn/index.js
+++ src/components/app/SignIn/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import { Container } from '@material-ui/core';
 
 import Form from './Form';
 
 export default function SignIn() {
+  const dispatch = useDispatch();
+
+  const onSubmit = values => {
+    dispatch({ type: 'SIGNIN', payload: values });
+  };
+
   return (
     <Container maxWidth="xs">
-      <Form />
+      <Form onSubmit={onSubmit} />
     </Container>
   );
 }
