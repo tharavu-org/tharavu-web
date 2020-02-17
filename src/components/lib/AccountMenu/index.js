9b5f53da2c905f20bde6cfb25a951b353cd81f33
Thanga Ganapathy <ganapathy888@gmail.com>
2020-02-17 20:40:45 +0530

    Add account link to header menu

--- src/components/lib/AccountMenu/index.js
+++ src/components/lib/AccountMenu/index.js
@@ -1,18 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
 import Icon from '@mdi/react';
 import { mdiAccount, mdiChevronDown } from '@mdi/js';
 import { Button, Menu, MenuItem } from '@material-ui/core';
+import { useHistory } from 'react-router-dom';
 
 import signout from '../../../utils/session';
 
 export default function AccountMenu() {
-  const [anchorEl, setAnchorEl] = React.useState(null);
+  const currentUser = useSelector(state => state.session.currentUser);
+  const [anchorEl, setAnchorEl] = useState(null);
+  const history = useHistory();
+
   const handleClick = event => {
     setAnchorEl(event.currentTarget);
   };
+
   const handleClose = () => {
     setAnchorEl(null);
   };
+
+  const handleAccountClick = () => {
+    handleClose();
+    if (currentUser.isMember) {
+      return history.push('/accounts/member');
+    }
+
+    return history.push('/accounts/user');
+  };
+
   return (
     <div>
       <Button
@@ -30,6 +46,7 @@ export default function AccountMenu() {
         open={Boolean(anchorEl)}
         onClose={handleClose}
       >
+        <MenuItem onClick={handleAccountClick}>Account</MenuItem>
         <MenuItem onClick={signout}>Sign out</MenuItem>
       </Menu>
     </div>
