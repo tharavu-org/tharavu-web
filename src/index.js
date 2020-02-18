d8031a2a9beb15e88b20c6c75574d0c897d4cb1f
Thanga Ganapathy <ganapathy888@gmail.com>
2020-02-18 19:49:00 +0530

    Add app global styles

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import 'typeface-roboto';
 
 import App from './App';
+import './App.css';
 
 const rootElement = document.getElementById('root');
 ReactDOM.render(<App />, rootElement);
