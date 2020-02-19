d89cefc5b4145578d543759332da494b2d8671e7
Thanga Ganapathy <ganapathy888@gmail.com>
2020-02-19 16:40:54 +0530

    Render errors based on data types in error box

--- src/components/lib/ErrorBox.js
+++ src/components/lib/ErrorBox.js
@@ -2,10 +2,19 @@ import React from 'react';
 import { Box } from '@material-ui/core';
 
 export default function ErrorBox({ error }) {
+  const renderErrors = () => {
+    if (typeof error === 'string') {
+      return error;
+    }
+    if (typeof error === 'object' && Array.isArray(error)) {
+      return error[0];
+    }
+  };
+
   if (error) {
     return (
       <Box color="white" bgcolor="error.main" p={2}>
-        {error}
+        {renderErrors()}
       </Box>
     );
   }
