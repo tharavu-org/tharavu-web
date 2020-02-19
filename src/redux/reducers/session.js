7c7eae782079f5202ac7a1dca22722b174d2ed6c
Thanga Ganapathy <ganapathy888@gmail.com>
2020-02-19 12:55:13 +0530

    Remove signin failed msg from session reducer

--- src/redux/reducers/session.js
+++ src/redux/reducers/session.js
@@ -2,7 +2,6 @@ import { handleActions } from 'redux-actions';
 
 const defaultState = {
   currentUser: JSON.parse(localStorage.getItem('currentUser')),
-  signinFailedMsg: null,
 };
 
 export default handleActions(
@@ -10,9 +9,6 @@ export default handleActions(
     SET_CURRENT_USER: (state, action) => {
       localStorage.setItem('currentUser', JSON.stringify(action.payload));
       return { ...state, currentUser: action.payload };
-    },
-    SIGNIN_FAILED: (state, action) => {
-      return { ...state, signinFailedMsg: action.payload };
     },
   },
   defaultState,
