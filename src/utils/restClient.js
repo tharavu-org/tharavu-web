734a9a098e010ca1c13a1eea6657f65d38cbc927
Thanga Ganapathy <ganapathy888@gmail.com>
2020-02-19 12:35:19 +0530

    Add custom APIError and throw it when needed

--- src/utils/restClient.js
+++ src/utils/restClient.js
@@ -1,13 +1,40 @@
+import signout from './session';
+
+class APIError extends Error {
+  constructor(data, ...params) {
+    // Pass remaining arguments (including vendor specific ones) to parent constructor
+    super(...params);
+
+    // Maintains proper stack trace for where our error was thrown (only available on V8)
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, APIError);
+    }
+
+    this.name = 'APIError';
+    // Custom debugging information
+    this.data = data;
+  }
+}
+
 export async function postAPI(url = '', data = {}) {
+  const { authToken } = JSON.parse(localStorage.getItem('currentUser'));
   const response = await fetch(process.env.REACT_APP_API_URL + url, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
+      Authorization: authToken,
     },
     body: JSON.stringify(data),
   });
+  if (response.status === 401) {
+    signout();
+  }
   if (!response.ok) {
-    throw new Error(response.statusText);
+    throw new APIError({
+      status: response.status,
+      statusText: response.statusText,
+      errors: await response.json(),
+    });
   }
   return response.json();
 }
