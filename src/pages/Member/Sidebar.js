9b61634d5346798458a63d5ac040e25cce406ca9
Thanga Ganapathy <ganapathy888@gmail.com>
2020-02-19 10:16:55 +0530

    Make member sidebar to full height

--- src/pages/Member/Sidebar.js
+++ src/pages/Member/Sidebar.js
@@ -1,7 +1,19 @@
 import React from 'react';
-import { MenuList, MenuItem, Paper } from '@material-ui/core';
+import { MenuList, MenuItem, Paper, makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles(theme => ({
+  menuList: {
+    height: `100%`,
+  },
+  paper: {
+    paddingTop: theme.spacing(1),
+    paddingBottom: theme.spacing(1),
+    height: `calc(100% - ${theme.spacing(2)}px)`,
+  },
+}));
 
 export default function Sidebar({ menu, onMenuChange }) {
+  const classes = useStyles();
   const menuOptions = ['dashboard', 'events', 'tags', 'members'];
 
   const renderMenuOptions = () => {
@@ -17,8 +29,8 @@ export default function Sidebar({ menu, onMenuChange }) {
     ));
   };
   return (
-    <MenuList>
-      <Paper square variant="outlined">
+    <MenuList disablePadding className={classes.menuList}>
+      <Paper className={classes.paper} square variant="elevation">
         {renderMenuOptions()}
       </Paper>
     </MenuList>
