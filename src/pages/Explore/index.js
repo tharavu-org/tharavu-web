e85c2aca31e0f4e0bf1904329f4267af368a94f1
Thanga Ganapathy <ganapathy888@gmail.com>
2020-03-14 23:52:48 +0530

    Get events from url params in explore

--- src/pages/Explore/index.js
+++ src/pages/Explore/index.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { makeStyles } from '@material-ui/core/styles';
+
 import Events from './Events';
 
 const useStyles = makeStyles(theme => ({
@@ -10,8 +12,19 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
-export default function Home() {
+export default function Explore({ location }) {
+  const dispatch = useDispatch();
   const classes = useStyles();
+  const regex = /\?(\w+)=(.*)/g;
+  const [, condition, value] = regex.exec(location.search);
+
+  useEffect(() => {
+    const conditionMap = { tags: 'tharavu_tags_name_eq' };
+    dispatch({
+      type: 'GET_EVENTS',
+      payload: { page: 1, searchParams: `${conditionMap[condition]}=${value}` },
+    });
+  }, [dispatch, condition, value]);
 
   return (
     <div className={classes.container}>
